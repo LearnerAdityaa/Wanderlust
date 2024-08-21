6fd919334f78616e327c9f8724478eae6183ee0a
LearnerAdityaa <159002878+LearnerAdityaa@users.noreply.github.com>
2024-08-21 22:22:32 +0545

    Update listing.js

--- routes/listing.js
+++ routes/listing.js
@@ -9,7 +9,7 @@ const multer = require("multer");
 const { storage } = require("../cloudConfig.js");
 const upload = multer({ storage });
 
-router.route("/listing").get(wrapAsync(listingController.index)).post(
+router.route("/").get(wrapAsync(listingController.index)).post(
   isLoggedIn,
 
   upload.single("listing[image]"),
