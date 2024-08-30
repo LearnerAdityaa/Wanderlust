0b013400121b4f53dce1d54b52562b2f9ddcd6a9
LearnerAdityaa <revengeforcoc@gmail.com>
2024-08-30 20:38:35 +0545

    vercel setup

--- init/index.js
+++ init/index.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
+const dbURL = process.env.ATLASDB_URL;
 
 main()
   .then(() => {
@@ -11,7 +12,7 @@ main()
   });
 
 async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
+  await mongoose.connect(dbURL);
 }
 
 const initDB = async () => {
